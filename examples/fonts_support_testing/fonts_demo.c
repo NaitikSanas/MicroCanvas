a558747696d8aa59fb5dde0b6cd6efe730f78514
Naitik <33337376+NaitikSanas@users.noreply.github.com>
2024-12-15 13:18:49 +0530

    Update fonts_demo.c

--- examples/fonts_support_testing/fonts_demo.c
+++ examples/fonts_support_testing/fonts_demo.c
@@ -4,7 +4,7 @@ void uCanvas_Run_Fonts_demo(void){
     start_uCanvas_engine();
     uCanvas_Scene_t* scene = New_uCanvas_Scene();
     uCanvas_set_active_scene(scene);
-    uCanvas_universal_obj_t* text = New_uCanvas_2DTextbox("Hello",20,20);
+    uCanvas_universal_obj_t* text = New_uCanvas_2DTextbox("Hello",20,70);
     text->font_properties.font_type = FONT_16G;
     text->font_properties.Font_Draw_Direction = uCanvas_Font_Dir_0;
     
